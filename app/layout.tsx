cb8f7ae375b4bc9cfff753733114b8053f3709e2
thenoobchad <noobchaddy@gmail.com>
2025-11-22 13:21:18 +0100

    fix:meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "build",
-  description: "build nft",
+	title: "build",
+	description: "build nft",
+	openGraph: {
+		images: [],
+	},
+	other: {
+		"fc:frame": "vNext",
+		"fc:frame:image": "https://buildnft.vercel.app/og-image.png",
+		"fc:frame:image:aspect_ratio": "1.91:1",
+		"fc:frame:button:1": "Mint Build NFT",
+		"fc:frame:button:1:action": "tx",
+		"fc:frame:button:1:target": "https://buildnft.vercel.app/api/mint-tx",
+		"fc:frame:post_url": "https://buildnft.vercel.app/api/success",
+	},
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 
 export default function Home() {
   return (
-    <main>Hello world.</main>
-  );
+		<>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://buildnft.vercel.app/og-image.png" />
+        <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+        <meta property="fc:frame:button:1" content="Mint Build NFT" />
+        <meta property="fc:frame:button:1:action" content="tx" />
+        <meta property="fc:frame:button:1:target" content="https://buildnft.vercel.app/api/mint-tx" />
+        <meta property="fc:frame:post_url" content="https://buildnft.vercel.app/api/success" />
+      </head>
+			<main>Hello world.</main>
+		</>
+	);
 }
